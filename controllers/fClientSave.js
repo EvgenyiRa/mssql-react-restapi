291d7fd29a7892cb09904782804326d4153351f0
EvgenyiRa <tryampampam@mail.ru>
2022-02-07 23:27:02 +0300

    Доработка: обработки данных от f-control-client;

--- controllers/fClientSave.js
+++ controllers/fClientSave.js
@@ -37,45 +37,53 @@
             contextE.execsql.push(context);
             const resquery = await execquery.find(contextE);
             //проверяем ограничения
-            let rows=resquery[0][0][0];
-            const timeAllClient=data.timeAll/1000;
-            data.access=true;
-            if (rows['TIME_ALL']<timeAllClient) {
-              data.access=false;
-            }
-            const userCntlID=rows['REP_USERS_CONTROL_ID'];
-            rows=resquery[1][0];
-            //console.log(data.winsActiveSum);
-            for (var i = 0; i < rows.length; i++) {
-              const rowOne=rows[i];
-              if (!!data.winsActiveSum[rowOne['PRC_NAME']]) {
-                const timeAllDeltaClient=data.winsActiveSum[rowOne['PRC_NAME']].timeAllDelta/1000;
-                data.winsActiveSum[rowOne['PRC_NAME']].access=true;
-                if (rowOne['LIM']<timeAllDeltaClient) {
-                    data.winsActiveSum[rowOne['PRC_NAME']].access=false;
+            let rows=resquery[0][0];
+            if (rows.length>0) {
+              rows=rows[0];
+              const timeAllClient=data.timeAll/1000;
+              data.access=true;
+              if (rows['TIME_ALL']>0) {
+                if (rows['TIME_ALL']<timeAllClient) {
+                  data.access=false;
                 }
               }
-            }
-            contextE.execsql=[];
-            context={};
-            context.params=[timeAllClient,req.body.date,(data.access)?1:0];
-            context.sql=`INSERT INTO REP_USR_CNTRL_SYS_STATE
-                                    (REP_USR_CNTRL_ID,TIME_ALL,DATE,ACCESS)
-                              VALUES (`+userCntlID+`,?,STR_TO_DATE(?, '%d-%m-%Y'),?) AS new
-                        ON DUPLICATE KEY UPDATE TIME_ALL=new.TIME_ALL,ACCESS=new.ACCESS;`;
-            contextE.execsql.push(context);
-            for (var key in data.winsActiveSum) {
-              const prcClient=data.winsActiveSum[key];
+              const userCntlID=rows['REP_USERS_CONTROL_ID'];
+              rows=resquery[1][0];
+              if (rows.length>0) {
+                //console.log(data.winsActiveSum);
+                for (var i = 0; i < rows.length; i++) {
+                  const rowOne=rows[i];
+                  if (!!data.winsActiveSum[rowOne['PRC_NAME']]) {
+                    const timeAllDeltaClient=data.winsActiveSum[rowOne['PRC_NAME']].timeAllDelta/1000;
+                    data.winsActiveSum[rowOne['PRC_NAME']].access=true;
+                    if (rowOne['LIM']<timeAllDeltaClient) {
+                        data.winsActiveSum[rowOne['PRC_NAME']].access=false;
+                    }
+                  }
+                }
+              }
+              contextE.execsql=[];
               context={};
-              context.params=[req.body.date,key,prcClient.pid,(prcClient.access)?1:0,prcClient.lastTimeProcess,prcClient.timeAll,parseInt(prcClient.timeAllDelta/1000),prcClient.timeAllUser];
-              context.sql=`INSERT INTO REP_USR_CNTRL_PRC_STATE
-                                      (REP_USR_CNTRL_ID,DATE,PRC_NAME,PID,ACCESS,LAST_TIME,TIME_ALL,TIME_ALL_DELTA,TIME_ALL_USR)
-                                VALUES (`+userCntlID+`, STR_TO_DATE(?, '%d-%m-%Y'),?,?,?,?,?,?,?) AS new
-                          ON DUPLICATE KEY UPDATE PID=new.PID,ACCESS=new.ACCESS,LAST_TIME=new.LAST_TIME,TIME_ALL=new.TIME_ALL,TIME_ALL_DELTA=new.TIME_ALL_DELTA,TIME_ALL_USR=new.TIME_ALL_USR;`;
+              context.params=[timeAllClient,req.body.date,(data.access)?1:0];
+              context.sql=`INSERT INTO REP_USR_CNTRL_SYS_STATE
+                                      (REP_USR_CNTRL_ID,TIME_ALL,DATE,ACCESS)
+                                VALUES (`+userCntlID+`,?,STR_TO_DATE(?, '%d-%m-%Y'),?) AS new
+                          ON DUPLICATE KEY UPDATE TIME_ALL=new.TIME_ALL,ACCESS=new.ACCESS;`;
               contextE.execsql.push(context);
+              for (var key in data.winsActiveSum) {
+                const prcClient=data.winsActiveSum[key];
+                context={};
+                context.params=[req.body.date,key,prcClient.pid,(prcClient.access)?1:0,prcClient.lastTimeProcess,prcClient.timeAll,parseInt(prcClient.timeAllDelta/1000),prcClient.timeAllUser];
+                context.sql=`INSERT INTO REP_USR_CNTRL_PRC_STATE
+                                        (REP_USR_CNTRL_ID,DATE,PRC_NAME,PID,ACCESS,LAST_TIME,TIME_ALL,TIME_ALL_DELTA,TIME_ALL_USR)
+                                  VALUES (`+userCntlID+`, STR_TO_DATE(?, '%d-%m-%Y'),?,?,?,?,?,?,?) AS new
+                            ON DUPLICATE KEY UPDATE PID=new.PID,ACCESS=new.ACCESS,LAST_TIME=new.LAST_TIME,TIME_ALL=new.TIME_ALL,TIME_ALL_DELTA=new.TIME_ALL_DELTA,TIME_ALL_USR=new.TIME_ALL_USR;`;
+                contextE.execsql.push(context);
+              }
+              //не дожидаемся завершения
+              execquery.find(contextE);
+
             }
-            //не дожидаемся завершения
-            execquery.find(contextE);
             //console.log(resquery[0][0],resquery[1][0]);
             return res.status(200).json({ data: data })
             //let rows=resquery.recordsets[0];
