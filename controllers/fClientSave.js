e7111fae5eef694a427014d5e992eb9199240b34
EvgenyiRa <tryampampam@mail.ru>
2022-02-07 22:07:04 +0300

    Доработка: обработки данных от f-control-client;

--- controllers/fClientSave.js
+++ controllers/fClientSave.js
@@ -6,7 +6,7 @@
             dbConfig = configs.database;;
 /*02*/
 /*03*/async function post(req, res, next) {
-        console.log('req.body',req.body);
+        //console.log('req.body',req.body);
         if ((!!req.body.repUserId)
               && (!!req.body.currentUser)
               && (!!req.body.data)
@@ -45,7 +45,7 @@
             }
             const userCntlID=rows['REP_USERS_CONTROL_ID'];
             rows=resquery[1][0];
-            console.log(data.winsActiveSum);
+            //console.log(data.winsActiveSum);
             for (var i = 0; i < rows.length; i++) {
               const rowOne=rows[i];
               if (!!data.winsActiveSum[rowOne['PRC_NAME']]) {
@@ -56,15 +56,26 @@
                 }
               }
             }
-            //contextE.execsql=[];
+            contextE.execsql=[];
             context={};
             context.params=[timeAllClient,req.body.date,(data.access)?1:0];
             context.sql=`INSERT INTO REP_USR_CNTRL_SYS_STATE
                                     (REP_USR_CNTRL_ID,TIME_ALL,DATE,ACCESS)
                               VALUES (`+userCntlID+`,?,STR_TO_DATE(?, '%d-%m-%Y'),?) AS new
                         ON DUPLICATE KEY UPDATE TIME_ALL=new.TIME_ALL,ACCESS=new.ACCESS;`;
+            contextE.execsql.push(context);
+            for (var key in data.winsActiveSum) {
+              const prcClient=data.winsActiveSum[key];
+              context={};
+              context.params=[req.body.date,key,prcClient.pid,(prcClient.access)?1:0,prcClient.lastTimeProcess,prcClient.timeAll,parseInt(prcClient.timeAllDelta/1000),prcClient.timeAllUser];
+              context.sql=`INSERT INTO REP_USR_CNTRL_PRC_STATE
+                                      (REP_USR_CNTRL_ID,DATE,PRC_NAME,PID,ACCESS,LAST_TIME,TIME_ALL,TIME_ALL_DELTA,TIME_ALL_USR)
+                                VALUES (`+userCntlID+`, STR_TO_DATE(?, '%d-%m-%Y'),?,?,?,?,?,?,?) AS new
+                          ON DUPLICATE KEY UPDATE PID=new.PID,ACCESS=new.ACCESS,LAST_TIME=new.LAST_TIME,TIME_ALL=new.TIME_ALL,TIME_ALL_DELTA=new.TIME_ALL_DELTA,TIME_ALL_USR=new.TIME_ALL_USR;`;
+              contextE.execsql.push(context);
+            }
             //не дожидаемся завершения
-            query.find(context);
+            execquery.find(contextE);
             //console.log(resquery[0][0],resquery[1][0]);
             return res.status(200).json({ data: data })
             //let rows=resquery.recordsets[0];
